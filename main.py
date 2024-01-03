c6e8df07a8fd445e87d71587f947bd61f717dbc2
Borjan Peovski <51805234+PeovskiB@users.noreply.github.com>
2024-01-03 01:17:49 +0100

    [REFACTOR] Created separate service for image captioning

--- main.py
+++ main.py
@@ -4,31 +4,9 @@ from PIL import Image
 import requests
 from io import BytesIO
 
-from transformers import AutoProcessor, Blip2ForConditionalGeneration, BlipProcessor, BlipForConditionalGeneration
-import torch
+import services.image_captioning as image_captioning
 
 app = FastAPI()
-
-device = "cuda" if torch.cuda.is_available() else "cpu"
-has_gpu = torch.cuda.is_available()
-print("GPU avaliable: " + str(has_gpu))
-
-#model = "Salesforce/blip2-opt-2.7b"
-model = "Salesforce/blip-image-captioning-large"
-
-# Load the Blip2 model with int8 quantization
-processor = BlipProcessor.from_pretrained(model)
-#model = BlipForConditionalGeneration.from_pretrained(model, device_map=device, load_in_8bit=has_gpu)
-model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large", torch_dtype=torch.float16).to(device)
-
-
-
-def generate_caption(image):
-    inputs = processor(image, return_tensors="pt").to(device, torch.float16)
-    generated_ids = model.generate(**inputs, max_new_tokens=20)
-    generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0].strip()
-    return generated_text
-
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile = File(...)):
@@ -37,7 +15,7 @@ async def create_upload_file(file: UploadFile = File(...)):
         img = Image.open(BytesIO(await file.read())).convert('RGB')
 
         # Generate caption for the image
-        caption = generate_caption(img)
+        caption = image_captioning.generate_caption(img)
 
         return {"filename": file.filename, "caption": caption}
     except Exception as e:

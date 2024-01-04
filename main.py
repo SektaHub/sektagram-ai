72fbf07e37a2256a507be42a786f0178dcee2b99
Borjan Peovski <51805234+PeovskiB@users.noreply.github.com>
2024-01-04 03:19:03 +0100

    [CHANGE] embed method now json

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
 from PIL import Image
 import requests
 from io import BytesIO
@@ -8,6 +9,11 @@ import services.image_captioning as image_captioning
 import services.sentence_embeddings as sentence_embeddings
 
 app = FastAPI()
+
+
+class SentenceInput(BaseModel):
+    sentence: str
+
 
 @app.post("/api/generateCaptionFromUpload/")
 async def generate_caption_from_upload(file: UploadFile = File(...)):
@@ -37,9 +43,13 @@ async def generate_caption_from_link(image_link: str):
 
 
 @app.post("/api/embed_sentence")
-async def embed_sentence(sentence: str):
-    embedding = sentence_embeddings.embed_sentence(sentence)
-    return {"embedding": embedding}
+async def embed_sentence(sentence_input: SentenceInput):
+    try:
+        sentence = sentence_input.sentence
+        embedding = sentence_embeddings.embed_sentence(sentence)
+        return {"embedding": embedding}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error processing sentence: {str(e)}")
 
 
 @app.get("/")
